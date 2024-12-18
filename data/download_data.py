e8734574ffb5f272e0c7bd4d6480b550266000f5
clarkmiyamoto <cmiyamot@usc.edu>
2024-12-18 14:24:43 -0500

    lazy commit

--- data/download_data.py
+++ data/download_data.py
@@ -6,7 +6,7 @@ test_dataset = datasets.MNIST(root='./', train=False, download=True, transform=t
 
 ### How to run on HPC
 '''
-$ srun --pty /bin/bash
+srun --pty /bin/bash
 
 singularity exec --overlay /scratch/cm6627/diffeo_cnn/my_env/overlay-15GB-500K.ext3:ro \
   /scratch/work/public/singularity/cuda12.3.2-cudnn9.0.0-ubuntu-22.04.4.sif \
