263f47e6511c7f52713d1883bea28d9cfc46189f
clarkmiyamoto <cmiyamot@usc.edu>
2024-12-18 14:24:40 -0500

    lazy commit

--- data/download_data.py
+++ data/download_data.py
@@ -3,3 +3,14 @@ from torchvision import datasets, transforms
 # Load MNIST dataset
 train_dataset = datasets.MNIST(root='./', train=True, download=True, transform=transform)
 test_dataset = datasets.MNIST(root='./', train=False, download=True, transform=transform)
+
+### How to run on HPC
+'''
+$ srun --pty /bin/bash
+
+singularity exec --overlay /scratch/cm6627/diffeo_cnn/my_env/overlay-15GB-500K.ext3:ro \
+  /scratch/work/public/singularity/cuda12.3.2-cudnn9.0.0-ubuntu-22.04.4.sif \
+  /bin/bash
+
+source /ext3/env.sh
+'''
