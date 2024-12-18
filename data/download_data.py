25b2d4422e8e608a33e8787616318c304518d23b
clarkmiyamoto <cmiyamot@usc.edu>
2024-12-18 14:26:44 -0500

    lazy commit

--- data/download_data.py
+++ data/download_data.py
@@ -1,8 +1,8 @@
 from torchvision import datasets, transforms
 
 # Load MNIST dataset
-train_dataset = datasets.MNIST(root='./', train=True, download=True, transform=transform)
-test_dataset = datasets.MNIST(root='./', train=False, download=True, transform=transform)
+train_dataset = datasets.MNIST(root='./', train=True, download=True)
+test_dataset = datasets.MNIST(root='./', train=False, download=True)
 
 ### How to run on HPC
 '''
